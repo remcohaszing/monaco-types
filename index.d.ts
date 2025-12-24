a06566745b3b0931896581b6fff33d34620bf708
Remco Haszing <remcohaszing@gmail.com>
2025-12-24 10:35:34 +0100

    Add missing JSDoc description for MonacoEditor

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,6 @@
 export type * from './monaco-editor.js'
 
+/**
+ * An instance of the `monaco-editor` or `monaco-editor-core` module.
+ */
 export type MonacoEditor = typeof import('./monaco-editor.js')
